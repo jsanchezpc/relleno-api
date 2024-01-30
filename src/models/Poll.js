37acd0dfca87b815d2fff4f3ceee878fbb71257d
Jorge <jsanchezpc@hotmail.com>
2024-01-30 17:10:32 +0100

    poll schema

---
+++ src/models/Poll.js
@@ -0,0 +1,47 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const answerSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+});
+
+const questionSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  question: {
+    type: String,
+    required: true,
+  },
+  answers: [answerSchema],
+  other: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+const pollSchema = new Schema({
+  id: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  questions: [questionSchema],
+});
+
+module.exports = mongoose.model("Poll", pollSchema);
